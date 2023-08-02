663313ad36b7261ee6d76009a9e4fe4b6796969e
Joseph <pmyjgs@nottingham.ac.uk>
2023-08-02 14:05:39 +0100

    Modify model3 to be disconnected model

--- markov_builder/models/thirty_models/model3.py
+++ markov_builder/models/thirty_models/model3.py
@@ -1,36 +1,41 @@
 from numpy import NaN
 
 from markov_builder.MarkovChain import MarkovChain
-from markov_builder.rate_expressions import negative_rate_expr, positive_rate_expr
 
 
 class model_03(MarkovChain):
     description = ""
-    states = ('O', 'C', 'I', 'IC')
-    rates = [('O', 'C', 'bm', 'am'),
-             ('I', 'IC', 'bm', 'am'),
-             ('O', 'I', 'ah', 'bh'),
-             ('C', 'IC', 'ah', 'bh')]
+    states = ('O1', 'C', 'O2', 'I')
+    rates = [('O1', 'C', 'bm', 'am'),
+             ('O2', 'I', 'ah', 'bh')]
 
-    open_state = 'O'
-    shared_variables_dict = {'V': NaN}
-    rate_dictionary = {'am': positive_rate_expr + ((2.26E-4, 6.99E-2),),
-                       'bm': negative_rate_expr + ((3.45E-5, 5.462E-2),),
-                       'ah': positive_rate_expr + ((0.08730, 8.91e-3),),
-                       'bh': negative_rate_expr + ((5.15e-3, 0.03158),),
+    rate_dictionary = {'am': ('p1 * exp(V * p2)',),
+                       'bm': ('p3 * exp(-V * p4)',),
+                       'ah': ('p5 * exp(V * p6)',),
+                       'bh': ('p7 * exp(-V * p8)',),
                        }
 
-    auxiliary_expression = "g_Kr * {} * (V - E_Kr)"
-    auxiliary_symbol = 'I_Kr'
-
-    auxiliary_params_dict = {'g_Kr': 0.1524,
-                             'E_Kr': -88
+    shared_variables_dict = {'V': NaN,
+                             'p1': 2.26e-4,
+                             'p2': 0.06990,
+                             'p3': 3.45e-5,
+                             'p4': 0.05462,
+                             'p5': 0.08730,
+                             'p6': 8.91e-3,
+                             'p7': 5.15e-3,
+                             'p8': 0.03158,
+                             'p9': 0.15240,
                              }
+
+    auxiliary_expression = "p9 * state_O1 * state_O2 * (V - E_Kr)"
+    auxiliary_symbol = 'I_Kr'
+    auxiliary_params_dict = {'E_Kr': -88}
+    open_state = 'O1'
 
     def __init__(self):
         super().__init__(states=self.states,
-                         open_state=self.open_state,
                          rates=self.rates,
+                         open_state=self.open_state,
                          rate_dictionary=self.rate_dictionary,
                          auxiliary_expression=self.auxiliary_expression,
                          auxiliary_symbol=self.auxiliary_symbol,
